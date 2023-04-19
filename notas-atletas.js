9bd99f0f5b72b17e6a6b42e54982e850b0183cb6
Gustavo12345liveira <oliveiragustavo.a067@gmail.com>
2023-04-18 23:08:16 -0300

    ajustada logica de calculo de media dos atletas

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,38 +1,43 @@
 let atletas = [
-    {
-      nome: "Cesar Abascal",
-      notas: [10, 9.34, 8.42, 10, 7.88]
-    },
-    {
-      nome: "Fernando Puntel",
-      notas:  [8, 10, 10, 7, 9.33]
-    },
-    {
-      nome: "Daiane Jelinsky",
-      notas: [7, 10, 9.5, 9.5, 8]
-    },
-    {
-      nome: "Bruno Castro",
-      notas: [10, 10, 10, 9, 9.5]
-    } 
+  {
+    nome: "Cesar Abascal",
+    notas: [10,10,7.88,8.42,9.34]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas:  [10,10,7,8,9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [10,7,8,9.5,9.5]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10,10,10,9,9.5]
+  } 
 ];
 
-function algumaCoisa(matriz) {
-    for (let i = 0; i < matriz.length; i++) {
-        media = obterMedia(matriz[i]['notas'])
-        console.log() 
-        console.log()
-        console.log()
-    }
+function ImprimirInformacoesAtletas(matriz) {
+  for (let i = 0; i < matriz.length; i++) {
+      media = obterMedia(matriz[i]['notas'])
+      console.log(" ");
+      console.log(`Atleta: ${matriz[i]['nome']}`) 
+      console.log(`Notas Obtidas: ${matriz[i]['notas']}`)
+      console.log(`Média Válida: ${media}`)
+  }
 }
 
 function obterMedia(listaNotas) {
-    listaNotas.shift()
-    listaNotas.pop()
-    let sum = listaNotas.reduce(function(a, b){
-      return a + b;
-    });
-    return sum / listaNotas.length
-} 
-    
-algumaCoisa(atletas)
+  somaTotalArray = 0
+  for (let i = 0; i < listaNotas.length; i++){
+    somaTotalArray += listaNotas[i]
+  }
+
+  totalMenosUltimo = somaTotalArray - listaNotas[listaNotas.length - 1];
+  totalParaCalculo = totalMenosUltimo - listaNotas[0]
+
+  comprimentoArrayParaCalculo = listaNotas.length - 2
+  return totalParaCalculo / comprimentoArrayParaCalculo;
+}
+
+ImprimirInformacoesAtletas(atletas)
